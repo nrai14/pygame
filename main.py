79ea78a91c179c490359dbdbb4b5fee0fde2aebe
nrai14 <nishadrai14@gmail.com>
2024-11-18 14:12:47 +0000

    added restriction for 60 FPS to use less of system resources

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pygame
-import constants 
+from constants import *
 
-pygame.init()
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True: 
         for event in pygame.event.get():
@@ -15,6 +14,10 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+
+        # Limiting framerate to 60 FPS to reduce CPU usage
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
